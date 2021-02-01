8c5f90a724b041678943cdffba25ee1442714698
EXvQ <zm19248@josai.ac.jp>
2021-02-01 15:17:46 +0000

    TR-40

---
+++ Queen.js
@@ -0,0 +1,2 @@
+function Q1() {document.getElementById('Queen').innerHTML = 'Queen(クイーン)';}
+function pandamanchange9() {document.getElementById('change p2-9').src = './image/pandaman2.JPG';}
---
+++ collection.js
@@ -0,0 +1,3 @@
+function imgchange1(){document.getElementById('collection').src = "./image/my collection2.jpg";}
+function imgchange2(){document.getElementById('collection').src = "./image/my collection1.jpg";}
+
