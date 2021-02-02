d0ae8c4cd3684b27ab66f6fe7eefe8fb8df31fd0
EXvQ <zm19248@josai.ac.jp>
2021-02-02 03:04:11 +0000

    TR-41

--- collection.js
+++ collection.js
@@ -1,3 +1,14 @@
-function imgchange1(){document.getElementById('collection').src = "./image/my collection2.jpg";}
-function imgchange2(){document.getElementById('collection').src = "./image/my collection1.jpg";}
+function imgchange1() { document.getElementById('collection').src = "./image/my collection2.jpg"; }
+function imgchange2() { document.getElementById('collection').src = "./image/my collection1.jpg"; }
 
+function imgchange() 
+{
+    if (document.getElementById('collection').src.includes("collection2")) 
+    {
+        document.getElementById('collection').src = "./image/my collection1.jpg";
+    } 
+    else 
+    {
+        document.getElementById('collection').src = "./image/my collection2.jpg";
+    }
+}
